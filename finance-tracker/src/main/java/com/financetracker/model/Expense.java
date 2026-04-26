e7ba308e49acd7f717ee3da1436a131345351a88
Sheshan4587 <sheshanthisal3@gmail.com>
2026-04-27 02:13:29 +0530

    initial commit - spring boot project setup

---
+++ finance-tracker/src/main/java/com/financetracker/model/Expense.java
@@ -0,0 +1,81 @@
+package com.financetracker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+//This annotation indicates that this class is a JPA entity, which means it will be mapped to a database table.
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String description;
+    private BigDecimal amount;
+    private String category;
+    private LocalDate date;
+
+    public Expense(UUID id, String description, BigDecimal amount, String category, LocalDate date) {
+        this.id = id;
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", category='" + category + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
