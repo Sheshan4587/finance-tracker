8d19ad90d4b21c32a5aacff7c183a560c19f9755
Sheshan4587 <sheshanthisal3@gmail.com>
2026-04-28 16:47:26 +0530

    add GlobalExceptionHandler

---
+++ finance-tracker/src/main/java/com/financetracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.financetracker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // This method is for handle
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+
+        ErrorResponse error = new ErrorResponse(404, ex.getMessage(), java.time.LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        ErrorResponse error = new ErrorResponse(500, ex.getMessage(),  java.time.LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+
+    }
+}
